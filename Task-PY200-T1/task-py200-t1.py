e46388831def10e4d6309b8c2f67e0f6e9f1d964
Ajeet Singh <singhajeet79@outlook.com>
2026-03-22 19:21:06 +0530

    test-py200-t1.ipynb done.

---
+++ Task-PY200-T1/task-py200-t1.py
@@ -0,0 +1,17 @@
+data = [10, 20, 20, 30, 40, 50, 20, 30]
+n = len(data)
+
+# ---1.MEAN ---
+total_sum = 0
+for x in data:
+    total_sum = total_sum +x
+mean = total_sum/n
+
+# ---2.Mediam ---
+
+
+
+
+
+print(f"Data: {data}")
+print(f"\nMean: {mean}")
